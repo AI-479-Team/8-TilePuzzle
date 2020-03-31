d493db0f064b419c1229a7addb03b011d006de73
Justin <Justin@LAPTOP-O00I69IQ>
2020-03-31 13:17:04 -0700

    Added Blank Tile movement And changed some things

--- src/base/Software/Node.java
+++ src/base/Software/Node.java
@@ -1,37 +1,94 @@
 package base.Software;
 
 public class Node {
-	
-	private int x, y;
+	//---------------------------------------
+	private int row, col;
 	private int[][] grid;
 	private int level;
 	private int misplacedTiles;
+	
+	//---------------------------------------
+
 	
 	
 	public Node() {
 		// TODO Auto-generated constructor stub
 		grid = new int[3][3];
 		setGrid();
+		setRowCol();
 	}
 	public void setGrid() {
-		int num = 0;
+		int num = 8;	// 0 will be the blank tile
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j ++) {
 				grid[i][j] = num;
-				num++;
+				num--;
+			}
+		}
+	}
+	
+	public void setRowCol() {
+		outerloop:
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (grid[i][j] == 0) {
+					row = i;
+					col = j;
+					break outerloop;
+				}
 			}
 		}
 	}
 	public void printGrid() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j ++) {
-				System.out.print(String.valueOf(grid[i][j]));
-				if(j == 2) {
-					System.out.println();	
-				}else {
-					System.out.print(" ");	
-				}
+				if (grid[i][j] == 0)
+					System.out.print(" _");		// prints whitespace then character : " 8"
+				else
+					System.out.print(" " + grid[i][j]); 
 			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public void moveBlankUp() {
+		if (row > 0) {
+			grid[row][col] = grid[--row][col];
+			grid[row][col] = 0;
+		}
+		else {
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE UP----------------------");
+		}
+	}
+	
+	public void moveBlankDown() {
+		if (row < 2) {
+			grid[row][col] = grid[++row][col];
+			grid[row][col] = 0;
+		}
+		else {
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE DOWN----------------------");
+		}
+	}
+
+	public void moveBlankRight() {
+		if (col < 2) {
+			grid[row][col] = grid[row][++col];
+			grid[row][col] = 0;
+		}
+		else {
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE RIGHT----------------------");
+		}
+	}	
+	
+	public void moveBlankLeft() {
+		if (col > 0) {
+			grid[row][col] = grid[row][--col];
+			grid[row][col] = 0;
+		}
+		else {
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE LEFT----------------------");
 		}
 	}
 	
--- src/base/Test/TestRun.java
+++ src/base/Test/TestRun.java
@@ -6,5 +6,22 @@ public class TestRun {
 	public static void main(String[] args) {
 		Node n1 = new Node();
 		n1.printGrid();
+		n1.moveBlankUp();
+		n1.printGrid();
+		n1.moveBlankUp();
+		n1.printGrid();
+		n1.moveBlankLeft();
+		n1.printGrid();
+		n1.moveBlankDown();
+		n1.printGrid();
+		n1.moveBlankLeft();
+		n1.printGrid();
+		n1.moveBlankRight();
+		n1.printGrid();
+		n1.moveBlankRight();
+		n1.printGrid();
+		n1.moveBlankDown();
+		n1.printGrid();
+		
 	}
 }
