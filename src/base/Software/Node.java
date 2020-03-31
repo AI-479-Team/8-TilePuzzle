6ecad70c5d6dd437ad462c64b4b6a5758ace69b1
Justin <Justin@LAPTOP-O00I69IQ>
2020-03-30 18:03:06 -0700

    test2

--- src/base/Software/Node.java
+++ src/base/Software/Node.java
@@ -12,7 +12,13 @@ public class Node {
 		// TODO Auto-generated constructor stub
 	}
 	public void setGrid() {
-		System.out.println("hi");
+		int num = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j ++) {
+				grid[i][j] = num;
+				num++;
+			}
+		}
 	}
 	
 
