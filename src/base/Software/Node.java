8e9347614bc67fe4d2958296b154e43c1ed0eb0c
Justin <Justin@LAPTOP-O00I69IQ>
2020-03-30 17:46:28 -0700

    commit1

---
+++ src/base/Software/Node.java
@@ -0,0 +1,19 @@
+package base.Software;
+
+public class Node {
+	
+	private int x, y;
+	private int[][] grid;
+	private int level;
+	private int misplacedTiles;
+	
+	
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
+	public void setGrid() {
+		System.out.println();
+	}
+	
+
+}
