6d053abb5d5daf2f93475e905301443c05835ffc
Justin <Justin@LAPTOP-O00I69IQ>
2020-04-03 20:44:56 -0700

    Almost done

--- src/base/Software/Node.java
+++ src/base/Software/Node.java
@@ -2,45 +2,47 @@ package base.Software;
 
 public class Node {
 	//---------------------------------------
-	private int row, col;
 	private int[][] grid;
+	private int row, col;
 	private int level;
 	private int misplacedTiles;
-	private int[][] LastState; //This would be used to check and make sure that the current node doesn't move back to it's previous state
+	//private int[][] LastState; //This would be used to check and make sure that the current node doesn't move back to it's previous state
+	private String fromDirection;	// This is the direction that was used to get to this Node.
+	private Node prevNode;		// the Node this Node came from
 	
 	//---------------------------------------
 	
 	public Node(int[][] matrix) {
-		// TODO Auto-generated constructor stub
 		grid = new int[3][3];
-		LastState = new int[3][3];
+	//	LastState = new int[3][3];
 		setGrid(matrix);
 		setRowCol();
-<<<<<<< HEAD
-	}
-	//-----------------------------SETTERS AND GETTERS-----------------------------
-	public void setGrid() {
-		int num = 8;	// 0 will be the blank tile
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j ++) {
-				grid[i][j] = num;
-				num--;
-			}
-		}
-=======
 		misplacedTiles = 0;
->>>>>>> branch 'master' of git@github.com:AI-479-Team/8-TilePuzzle.git
+		fromDirection = "none"; 
+		level = 0;
+		prevNode = null;
+	}
+	// Copy Constructor
+	public Node(Node node) {
+		this.row = node.getRow();
+		this.col = node.getCol();
+		this.level = node.getLevel();
+		this.misplacedTiles = node.getMisplacedTiles();
+		this.fromDirection = node.getFromDirection();
+		this.prevNode = node.getPrevNode();
+		this.copyGrid(node.getGrid());
 	}
 	
-	public void setLastState(int[][] matrix) {
-		LastState = matrix;
-	}
 	
+	//---------------------------------------------BEGIN: GETTERS/SETTERS----------------------------------
 	public void setGrid(int[][] matrix) {
 		grid = matrix;
 	}
+	public int[][] getGrid() {
+		return grid;
+	}
 	
-	public void setRowCol(){
+	public void setRowCol() {
 		outerloop:
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -52,6 +54,129 @@ public class Node {
 			}
 		}
 	}
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	
+	public void setLevel(int l) {
+		level = l;
+	}
+	public int getLevel() {
+		return level;
+	}
+	
+	public void calculateMisplacedTiles(int[][] GoalState) {
+		misplacedTiles = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j ++) {
+				if ((GoalState[i][j] != 0) && (GoalState[i][j] != grid[i][j]))
+					misplacedTiles++;
+			}
+		}
+	}
+	public int getMisplacedTiles() {
+		return misplacedTiles;
+	}
+	
+	
+	public void setFromDirection(String d) {
+		fromDirection = d;
+	}
+	public String getFromDirection() {
+		return fromDirection;
+	}
+	
+	public void setPrevNode(Node node) {
+		prevNode = node;
+	}
+	public Node getPrevNode() {
+		return prevNode;
+	}
+	
+	public int getManhattanCost() {
+		
+		return 1;
+	}
+	public int getMisplacedTilesCost() {
+		return level + misplacedTiles;
+	}
+	//---------------------------------------------END: GETTERS/SETTERS----------------------------------
+	
+	//---------------------------------------------BEGIN: BLANK TILE MOVEMENT----------------------------------
+	public void moveBlankUp() {
+		if (row > 0) {
+			grid[row][col] = grid[--row][col];
+			grid[row][col] = 0;
+			incrementLevel();
+			setFromDirection("up");
+		}
+		else
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE UP----------------------");
+	}
+	
+	public void moveBlankDown() {
+		if (row < 2) {
+			grid[row][col] = grid[++row][col];
+			grid[row][col] = 0;
+			incrementLevel();
+			setFromDirection("down");
+		}
+		else
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE DOWN----------------------");
+	}
+
+	public void moveBlankRight() {
+		if (col < 2) {
+			grid[row][col] = grid[row][++col];
+			grid[row][col] = 0;
+			incrementLevel();
+			setFromDirection("right");
+		}
+		else 
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE RIGHT----------------------");
+	}	
+	
+	public void moveBlankLeft() {
+		if (col > 0) {
+			grid[row][col] = grid[row][--col];
+			grid[row][col] = 0;
+			incrementLevel();
+			setFromDirection("left");
+		}
+		else
+			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE LEFT----------------------");
+	}
+	//---------------------------------------------END: BLANK TILE MOVEMENT----------------------------------
+	
+	//---------------------------------------------BEGIN: BLANK TILE CHECKING----------------------------------
+	public boolean checkBlankUp() {
+		if (row > 0 && !fromDirection.equalsIgnoreCase("down")) 
+			return true;
+		return false;
+	}
+	
+	public boolean checkBlankDown() {
+		if (row < 2 && !fromDirection.equalsIgnoreCase("up"))
+			return true;
+		return false;
+	}
+
+	public boolean checkBlankRight() {
+		if (col < 2 && !fromDirection.equalsIgnoreCase("left"))
+			return true;
+		return false;
+	}	
+	
+	public boolean checkBlankLeft() {
+		if (col > 0 && !fromDirection.equalsIgnoreCase("right"))
+			return true;
+		return false;
+	}
+	//---------------------------------------------END: BLANK TILE CHECKING----------------------------------
+	
 	public void printGrid() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j ++) {
@@ -64,104 +189,19 @@ public class Node {
 		}
 		System.out.println();
 	}
-<<<<<<< HEAD
 	
+	public void incrementLevel() {
+		level++;
+	}
+	public void decrementLevel() {
+		level--;
+	}
 	
-=======
-	public void calculateMisplacedTiles(int[][] GoalState) {
+	public void copyGrid(int[][] g) {
+		grid = new int[3][3];
 		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j ++) {
-				if ((GoalState[i][j] != 0) && (GoalState[i][j] != grid[i][j]))
-					misplacedTiles++;
-			}
+			for (int j = 0; j < 3; j++)
+				grid[i][j] = g[i][j];
 		}
 	}
-	
-	public int getMisplacedTiles() {
-		return misplacedTiles;
-	}
-	
-	public int[][] getGrid() {
-		return grid;
-	}
->>>>>>> branch 'master' of git@github.com:AI-479-Team/8-TilePuzzle.git
-
-	//---------------------------------------------BEGIN: BLANK TILE MOVEMENT----------------------------------
-	public void moveBlankUp() {
-		if (row > 0) {
-			grid[row][col] = grid[--row][col];
-			grid[row][col] = 0;
-		}
-		else
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE UP----------------------");
-	}
-	
-	public void moveBlankDown() {
-		if (row < 2) {
-			grid[row][col] = grid[++row][col];
-			grid[row][col] = 0;
-		}
-		else
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE DOWN----------------------");
-	}
-
-	public void moveBlankRight() {
-		if (col < 2) {
-			grid[row][col] = grid[row][++col];
-			grid[row][col] = 0;
-		}
-		else 
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE RIGHT----------------------");
-	}	
-	
-	public void moveBlankLeft() {
-		if (col > 0) {
-			grid[row][col] = grid[row][--col];
-			grid[row][col] = 0;
-		}
-		else
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE LEFT----------------------");
-	}
-	//---------------------------------------------END: BLANK TILE MOVEMENT----------------------------------
-	public int checkBlankUp() {
-		if (row > 0) {
-			return 1;
-		}
-		else {
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE UP----------------------");
-			return 0;
-		}
-	}
-	
-	public int checkBlankDown() {
-		if (row < 2) {
-			return 1;
-		}
-		else {
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE DOWN----------------------");
-			return 0;
-		}
-	}
-
-	public int checkBlankRight() {
-		if (col < 2) {
-			return 1;
-		}
-		else {
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE RIGHT----------------------");
-			return 0;
-		}
-	}	
-	
-	public int checkBlankLeft() {
-		if (col > 0) {
-			return 1;
-		}
-		else {
-			System.out.println("----------------------ERROR: CAN'T MOVE BLANK SPACE LEFT----------------------");
-			return 0;
-		}
-	}
-	
-	
 }
--- src/base/Test/TestRun.java
+++ src/base/Test/TestRun.java
@@ -1,7 +1,13 @@
 package base.Test;
-
+import java.util.Scanner;
 import base.Software.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+
 
 public class TestRun {
 	public static int[][] setInitialState(){
@@ -10,22 +16,23 @@ public class TestRun {
 		
 		/*			Initial State			*/
 		/*Row 1*/
-		InitialState[0][0] = 4;
-		InitialState[0][1] = 3;
-		InitialState[0][2] = 1;
+		InitialState[0][0] = 1;
+		InitialState[0][1] = 2;
+		InitialState[0][2] = 3;
 		
 		/*Row 2*/
-		InitialState[1][0] = 8;
-		InitialState[1][1] = 0;
-		InitialState[1][2] = 7;
+		InitialState[1][0] = 4;
+		InitialState[1][1] = 5;
+		InitialState[1][2] = 0;
 		
 		/*Row 3*/
-		InitialState[2][0] = 6;
-		InitialState[2][1] = 2;
-		InitialState[2][2] = 5;
+		InitialState[2][0] = 7;
+		InitialState[2][1] = 8;
+		InitialState[2][2] = 6;
 		
 		return InitialState;
 	}
+	
 	public static int[][] setGoalState(){
 		/*			Goal State			*/
 		int[][] GoalState;
@@ -38,20 +45,127 @@ public class TestRun {
 		
 		/*Row 2*/
 		GoalState[1][0] = 4;
-		GoalState[1][1] = 0;
-		GoalState[1][2] = 5;
+		GoalState[1][1] = 5;
+		GoalState[1][2] = 6;
 		
 		/*Row 3*/
-		GoalState[2][0] = 6;
-		GoalState[2][1] = 7;
+		GoalState[2][0] = 7;
+		GoalState[2][1] = 0;
 		GoalState[2][2] = 8;
 		
 		return GoalState; 
 	}
 	
+	public static boolean isSolvable(int[] initialArray, Map<Integer, Integer> treeMap) {
+	    int count = 0; 
+	    for (int i = 0; i < 7; i++) {
+	    	for (int j = i + 1; j < 8; j++) {
+	    		if (treeMap.get(initialArray[i]) < treeMap.get(initialArray[j]))
+	    			count++;
+	    	}
+	    	
+	    }
+	    return ((count & 1) == 0); 
+	} 	
+	
+	public void dynamic(Node n, ArrayList<Node> exploredStates, int searchType) {
+		for (Node exploredNode : exploredStates) {
+			if (Arrays.deepEquals(n.getGrid(), exploredNode.getGrid()) ) {
+				if (searchType == 0) {
+					if (n.getMisplacedTilesCost() <= exploredNode.getMisplacedTilesCost() )
+						exploredStates.add(n);
+				}
+				else {
+					if (n.getManhattanCost() <= exploredNode.getManhattanCost())
+						exploredStates.add(n);		
+				}
+			}
+		}
+	}
+	
+	public static void addToNextStates(Node n, ArrayList<Node> ExploredStates, ArrayList<Node> NextStates) {
+		boolean isIn = false;
+		
+		for (Node exploredNode : ExploredStates) {
+			if (Arrays.deepEquals(n.getGrid(), exploredNode.getGrid())) {
+				isIn = true;
+				break;
+			}
+		}
+		if (!isIn)
+			NextStates.add(n);
+	}
+	
+	public static void sortNextStates(ArrayList<Node> NextStates) {
+		int min = 99999;
+		int minIndex = 0;
+		for (int i = 0; i < NextStates.size(); i++) {
+			if (NextStates.get(i).getMisplacedTilesCost() < min) {
+				min = NextStates.get(i).getMisplacedTilesCost();
+				minIndex = i;
+			}
+		}
+		Collections.swap(NextStates, minIndex, 0);
+	}
+	
+	public static void printMoves(Node node) {
+		Node current = node;
+		ArrayList<String> moves = new ArrayList<String>();
+		while (current.getPrevNode() != null) {
+			moves.add(0, current.getFromDirection());
+			current = current.getPrevNode();
+		}
+		
+		System.out.println("Solution: ");
+		for (String move : moves) {
+			System.out.println("Move blank " + move);
+		}
+	}
+	
 	public static void main(String[] args) {
+		/*
+		Scanner row1 = new Scanner(System.in);
+		System.out.println("Enter the initial state: ");
+		*/
+		
 		
 		Node InitialNode = new Node(setInitialState());
+		int[][] goalState = setGoalState();
+		
+		//-------------------------Setting up TreeMap to determine solvability-------------
+		int[] initialArray = new int[8];
+		int[] goalArray = new int[8];
+		Map<Integer, Integer> treeMap = new TreeMap<Integer, Integer>();
+		
+		int ia = 0;
+		int ga = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (InitialNode.getGrid()[i][j] != 0) {
+					initialArray[ia] = InitialNode.getGrid()[i][j];
+					ia++;
+				}
+				if (goalState[i][j] != 0) {
+					goalArray[ga] = goalState[i][j];
+					ga++;
+				}
+			}
+		}
+		
+		for (int i = 0; i < 8; i++) {
+			treeMap.put(goalArray[i], i+1);
+		}
+		
+		boolean solvable = isSolvable(initialArray, treeMap);
+		System.out.println("is solvable: " + solvable);
+		//-------------------------------------------------------------------------------------
+		
+		
+		System.out.println(Arrays.toString(initialArray));
+		System.out.println(Arrays.toString(goalArray));
+		
+		//System.out.println("is equal: " + Arrays.deepEquals(InitialNode.getGrid(), currentNode.getGrid()));
+		
 		System.out.println("Grid of Initial State");
 		InitialNode.printGrid();
 		
@@ -60,73 +174,64 @@ public class TestRun {
 		GoalNode.printGrid();
 		
 		ArrayList<Node> ExploredStates = new ArrayList<Node>();
-		ArrayList<Node> CurrentStates = new ArrayList<Node>();
+		Node CurrentState = null;
 		ArrayList<Node> NextStates = new ArrayList<Node>();
 		
-		boolean flag = false;
-		int level = 1;
 		
-		while(!flag){ //This should be while(!CurrentNode!=GoalNode || !Impossible)
+		boolean flag = false;
+		
+		while(!flag) { //This should be while(!CurrentNode!=GoalNode || !Impossible)
 			if(ExploredStates.size()== 0) {
-				CurrentStates.add(InitialNode);
+				CurrentState = InitialNode;
 			}
-			for(Node CurrentState : CurrentStates) {
-				if(CurrentState.getGrid() != GoalNode.getGrid()) {
-					if(CurrentState.checkBlankDown() == 1) {
-						Node downNode = new Node(CurrentState.getGrid());
-						downNode.moveBlankDown();// Before moving tile, need to check if move would revert to old state
-						NextStates.add(downNode);
-						ExploredStates.add(downNode);
-					}
-					if(CurrentState.checkBlankUp() == 1) {
-						Node upNode = new Node(CurrentState.getGrid());
-						upNode.moveBlankDown();// Before moving tile, need to check if move would revert to old state
-						NextStates.add(upNode);
-						ExploredStates.add(upNode);
-					}
-					if(CurrentState.checkBlankLeft() == 1) {
-						Node leftNode = new Node(CurrentState.getGrid());
-						leftNode.moveBlankDown();// Before moving tile, need to check if move would revert to old state
-						NextStates.add(leftNode);
-						ExploredStates.add(leftNode);
-					}
-					if(CurrentState.checkBlankRight() == 1) {
-						Node rightNode = new Node(CurrentState.getGrid());
-						rightNode.moveBlankDown();// Before moving tile, need to check if move would revert to old state
-						NextStates.add(rightNode);
-						ExploredStates.add(rightNode);
-					}
+			CurrentState.printGrid();
+			if (!Arrays.deepEquals(CurrentState.getGrid(), goalState)) {
+				ExploredStates.add(CurrentState);
+				if(CurrentState.checkBlankDown()) {
+					Node downNode = new Node(CurrentState);
+					downNode.moveBlankDown();	// Before moving tile, need to check if move would revert to old state
+					downNode.calculateMisplacedTiles(goalState); // THIS MAY NEED TO BE CHANGED
+					downNode.setPrevNode(CurrentState);
+					addToNextStates(downNode, ExploredStates, NextStates);
+				//	downNode.printGrid();
 				}
-				else {
-					flag = true;
+				if(CurrentState.checkBlankUp()) {
+					Node upNode = new Node(CurrentState);
+					upNode.moveBlankUp();// Before moving tile, need to check if move would revert to old state
+					upNode.calculateMisplacedTiles(goalState);
+					upNode.setPrevNode(CurrentState);
+					addToNextStates(upNode, ExploredStates, NextStates);
+			//		upNode.printGrid();
 				}
-				
+				if(CurrentState.checkBlankLeft()) {
+					Node leftNode = new Node(CurrentState);
+					leftNode.moveBlankLeft();// Before moving tile, need to check if move would revert to old state
+					leftNode.calculateMisplacedTiles(goalState);
+					leftNode.setPrevNode(CurrentState);
+					addToNextStates(leftNode, ExploredStates, NextStates);
+			//		leftNode.printGrid();
+			//		System.out.println(leftNode.getMisplacedTilesCost());
+				}
+				if(CurrentState.checkBlankRight()) {
+					Node rightNode = new Node(CurrentState);
+					rightNode.moveBlankRight();// Before moving tile, need to check if move would revert to old state
+					rightNode.calculateMisplacedTiles(goalState);
+					rightNode.setPrevNode(CurrentState);
+					addToNextStates(rightNode, ExploredStates, NextStates);
+		//			rightNode.printGrid();
+				}
 			}
-			int minimum_cost = 0;
-			int temp_cost = 0;
-			ArrayList<Node> PotentialStates = new ArrayList<Node>();
+			else {
+				printMoves(CurrentState);
+				break;
+			}
 			
-			for(int i = 0; i< NextStates.size(); i++) {
-				if(i == 0) {
-					minimum_cost = NextStates.get(0).getMisplacedTiles() + level;
-					PotentialStates.add(NextStates.get(0));
-				}
-				else {
-					temp_cost = NextStates.get(i).getMisplacedTiles() + level;
-					if(temp_cost<minimum_cost) {
-						minimum_cost = temp_cost;
-						PotentialStates = new ArrayList<Node>();
-						PotentialStates.add(NextStates.get(i));
-					}
-					else if(temp_cost == minimum_cost) {
-						PotentialStates.add(NextStates.get(i));
-					}
-				}
+			if (!NextStates.isEmpty()) {
+				sortNextStates(NextStates);
+				CurrentState = NextStates.remove(0);
 			}
-			CurrentStates = new ArrayList<Node>();
-			NextStates = new ArrayList<Node>();
-			CurrentStates = PotentialStates;
-			level++;
 		}
+		
 	}
+
 }
